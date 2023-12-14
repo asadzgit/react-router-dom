71a6fe0d40c273de81e36e0ba565670b8c5e6e73
Muhammad Asad <asadmahmood41999@gmail.com>
2023-12-14 16:29:17 +0500

    moved styling to js object

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,18 @@
 import { NavLink } from "react-router-dom";
 // NavLink receives a 'active' class by default
 export const Navbar = () => {
+  const NavLinkStyles = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? 'bold' : 'normal',
+      textDecoration: isActive ? 'none' : 'underline',
+    }
+
+  }
+
   return (
     <nav>
-      <NavLink to='/'>Home</NavLink>
-      <NavLink to='/about'>About</NavLink>
+      <NavLink style={NavLinkStyles} to='/'>Home</NavLink>
+      <NavLink style={NavLinkStyles} to='/about'>About</NavLink>
     </nav>
   )
 }
