b136f520a42f55e11678f2161b2d48d26c1823cc
Muhammad Asad <asadmahmood41999@gmail.com>
2023-12-14 17:00:40 +0500

    Navigate to pages and Go back

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
 import { Navbar } from "./components/Navbar";
+import { OrderSummary } from "./components/OrderSummary";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     {/* we are doing this for our within-app routes. but for links to external resources, use anchor tag */}
       <Route path="/" element={<Home />} />
       <Route path="about" element={<About />} />
+      <Route path="order-summary" element={<OrderSummary />} />
     </Routes>
     </>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate()
+
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+      <button onClick={() => navigate('order-summary')}>Place order</button>
+    </>
   );
 }
 
