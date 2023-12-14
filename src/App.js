77eaac6fa3f13cbf7a6300532687e76c665835ed
Muhammad Asad <asadmahmood41999@gmail.com>
2023-12-14 16:17:30 +0500

    Added nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
+    <>
+    <Navbar />
     <Routes>
+    {/* we are doing this for our within-app routes. but for links to external resources, use anchor tag */}
       <Route path="/" element={<Home />} />
       <Route path="about" element={<About />} />
     </Routes>
+    </>
   );
 }
 
