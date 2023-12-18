33be0e5b88b950a8de136b634490b56b7c1c0fb4
Muhammad Asad <asadmahmood41999@gmail.com>
2023-12-18 13:17:52 +0500

    URL params

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,7 +1,7 @@
-import { useParams } from "react-router-dom";
-// useParams hook
+import { useParams } from "react-router-dom"; // useParams hook from router
+
 export const UserDetails = () => {
   const params = useParams() // it returns an object of key-value pairs
-  const userId = params.userId;
+  const userId = params.userId
   return <h1>Details about {userId}</h1>;
 };
