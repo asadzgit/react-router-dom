441b361d72c911cdcefde04da625f9f2189aa2ce
Muhammad Asad <asadmahmood41999@gmail.com>
2023-12-18 15:18:20 +0500

    relative route

--- src/components/Products.js
+++ src/components/Products.js
@@ -7,8 +7,13 @@ export const Products = () => {
         <input type="search" placeholder=" search products"></input>
       </div>
       <nav>
+        {/* relative lnks dont use slash  */}
         <Link to='featured'>Featured</Link>
         <Link to='new'>New</Link>
+        {/* Absolute links like following will not work because ot will search from root*/}
+        {/* <Link to='/featured'>Featured</Link> */}
+        {/* it will work this way */}
+        {/* <Link to='/products/new'>New</Link> */}
       </nav>
       <Outlet />
     </>
