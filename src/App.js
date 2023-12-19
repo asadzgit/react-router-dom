24b6e1370f9bc6fc814da3d9e0d1780cb4797291
Muhammad Asad <asadmahmood41999@gmail.com>
2023-12-19 12:05:20 +0500

    protected rutes and authentication

--- src/App.js
+++ src/App.js
@@ -11,39 +11,45 @@ import { FeaturedProducts } from "./components/FeaturedProducts";
 import { NewProducts } from "./components/NewProducts";
 import { Users } from "./components/Users";
 import { UserDetails } from "./components/UserDetails";
+import { Profile } from './components/Profile';
+import { AuthProvider } from './components/auth';
+import { Login } from './components/Login';
+import { RequireAuth } from './components/RequireAuth';
 const LazyAbout = React.lazy(() => import("./components/About"));
 
 function App() {
   return (
-    <>
-    <Navbar />
-    <Routes>
-    {/* we are doing this for our within-app routes. but for links to external resources, use anchor tag */}
-      <Route path="/" element={<Home />} />
-      <Route
-        path="about"
-        element={
-          <React.Suspense fallback='Loading...'>
-            <LazyAbout />
-          </React.Suspense>
-        }
-      />
-      <Route path="order-summary" element={<OrderSummary />} />
+    <AuthProvider>
+      <Navbar />
+      <Routes>
+      {/* we are doing this for our within-app routes. but for links to external resources, use anchor tag */}
+        <Route path="/" element={<Home />} />
+        <Route
+          path="about"
+          element={
+            <React.Suspense fallback='Loading...'>
+              <LazyAbout />
+            </React.Suspense>
+          }
+        />
+        <Route path="order-summary" element={<OrderSummary />} />
 
-      {/* Nested Route : A common use case is of layout containing its nested pages */}
-      <Route path="products" element={<Products />}>
-        {/* index route : when making one of the nested route to be rendered at the parent url  */}
-        <Route index element={<FeaturedProducts />} />
-        <Route path="featured" element={<FeaturedProducts />}></Route>
-        <Route path="new" element={<NewProducts />}></Route>
-      </Route>
-      <Route path="users" element={<Users />}>
-        <Route path=":userId" element={<UserDetails />}></Route>
-        <Route path="admin" element={<Admin />}></Route>
-      </Route>
-      <Route path="*" element={<NoPage />} />
-    </Routes>
-    </>
+        {/* Nested Route : A common use case is of layout containing its nested pages */}
+        <Route path="products" element={<Products />}>
+          {/* index route : when making one of the nested route to be rendered at the parent url  */}
+          <Route index element={<FeaturedProducts />} />
+          <Route path="featured" element={<FeaturedProducts />}></Route>
+          <Route path="new" element={<NewProducts />}></Route>
+        </Route>
+        <Route path="users" element={<Users />}>
+          <Route path=":userId" element={<UserDetails />}></Route>
+          <Route path="admin" element={<Admin />}></Route>
+        </Route>
+        <Route path="profile" element={<RequireAuth><Profile /></RequireAuth>}></Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "./auth";
 // NavLink receives a 'active' class by default
 export const Navbar = () => {
   const NavLinkStyles = ({ isActive }) => {
@@ -6,14 +7,21 @@ export const Navbar = () => {
       fontWeight: isActive ? 'bold' : 'normal',
       textDecoration: isActive ? 'none' : 'underline',
     }
-
   }
+
+  const auth = useAuth()
 
   return (
     <nav className="primary-nav">
       <NavLink style={NavLinkStyles} to='/'>Home</NavLink>
       <NavLink style={NavLinkStyles} to='/about'>About</NavLink>
       <NavLink style={NavLinkStyles} to='/products'>Products</NavLink>
+      <NavLink style={NavLinkStyles} to='/profile'>Profile</NavLink>
+      {!auth.user && (
+        <NavLink style={NavLinkStyles} to='/login'>
+          Login
+        </NavLink>
+      )}
     </nav>
   )
 }
